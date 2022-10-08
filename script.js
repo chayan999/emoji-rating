0ac0bd8b71f22766468ee468154273a17e7a6546
Chayan Chandan das <chayanchandandas@gmail.com>
2022-10-08 20:41:06 +0600

    push js file

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const stersEl = document.querySelectorAll(".fas");
+const emojisEl = document.querySelectorAll('.far')
+const color = ["red", "orange", "lightblue", "lightgreen", "green"]
+stersEl.forEach((ster, index) => {
+    ster.addEventListener('click', () => {
+        updateRating(index);
+    })
+});
+
+function updateRating(index) {
+    stersEl.forEach((ster, inx) => {
+        if (inx < index + 1) {
+            ster.classList.add("active")
+        } else {
+            ster.classList.remove("active")
+        }
+    });
+
+    emojisEl.forEach((emoji) => {
+        emoji.style.transform = `translateX(-${index * 50}px)`;
+        emoji.style.color = color[index]
+    })
+
+
+}
